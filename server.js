a0b1b80ae6a09dff389dc90e3f3805d3b7c88245
Andrew <parryandrew47@gmail.com>
2026-05-23 15:41:05 -0600

    feat: add MongoDB connection and users CRUD endpoints
    Wire up MongoDB via dotenv-configured connection string, add getAll and getSingle user controllers, and expose GET /users and GET /users/:id routes.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => [console.log(`Running on localhost:${port}`)]);
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => [console.log(`DB is up and node running on localhost:${port}`)]);
+    }
+});
