d0e6a9bc5e6ae1463d43f900780f822ebd0afe66
Andrew <parryandrew47@gmail.com>
2026-05-23 16:01:59 -0600

    feat: adding rest test for project assignment 1

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,18 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db('users').collection('users').find();
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
+    const result = await mongodb.getDatabase().db('project1').collection('users').find();
+    const users = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(users);
 };
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db('users').collection('users').find({ _id: userId });
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
-    });
-
+    const result = await mongodb.getDatabase().db('project1').collection('users').find({ _id: userId });
+    const users = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(users[0]);
 };
 
 module.exports = {
