3f4229634fbccd40f937bbbecb6f1176d1bb2a57
Andrew <parryandrew47@gmail.com>
2026-05-23 17:27:55 -0600

    feat: complete CRUD endpoints and add Swagger documentation
    - Implement createUser, updateUser, and deleteUser controller functions
    - Add POST, PUT, DELETE routes to users router
    - Add body-parser middleware for JSON request bodies
    - Add CORS headers to server
    - Add swagger-autogen and swagger-ui-express; generate swagger.json
    - Expose interactive API docs at /api-docs

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    // #swagger.tags = ['Users']
     const result = await mongodb.getDatabase().db('project1').collection('users').find();
     const users = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
@@ -9,6 +10,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+    // #swagger.tags = ['Users']
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db('project1').collection('users').find({ _id: userId });
     const users = await result.toArray();
@@ -16,7 +18,56 @@ const getSingle = async (req, res) => {
     res.status(200).json(users[0]);
 };
 
+const createUser = async (req, res) => {
+    // #swagger.tags = ['Users']
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const result = await mongodb.getDatabase().db('project1').collection('users').insertOne(user);
+    if (result.acknowledged) {
+        res.status(201).json({ id: result.insertedId });
+    } else {
+        res.status(500).json({ error: 'Failed to create user.' });
+    }
+};
+
+const updateUser = async (req, res) => {
+    // #swagger.tags = ['Users']
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const result = await mongodb.getDatabase().db('project1').collection('users').replaceOne({ _id: userId }, user);
+    if (result.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json({ error: 'Failed to update user.' });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    // #swagger.tags = ['Users']
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db('project1').collection('users').deleteOne({ _id: userId });
+    if (result.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json({ error: 'Failed to delete user.' });
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
-}
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
+};
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    })
+    .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
     } else {
-        app.listen(port, () => [console.log(`DB is up and node running on localhost:${port}`)]);
+        app.listen(port, () => console.log(`DB is up and node running on localhost:${port}`));
     }
 });
